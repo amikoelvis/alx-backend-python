9be087246cf4deb87cb66c799e1f9dad00ea95b5
Amiko Elvis <amikoelvis@gmail.com>
2025-07-29 11:39:05 +0300

    kdhdhh

---
+++ Django-signals_orm-0x04/messaging/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from messaging.models import Message
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['receiver', 'content', 'parent_message']
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.db.models import Prefetch
 from messaging.models import Message
+from messaging.forms import MessageForm
 
 @login_required
 def delete_user(request):
@@ -16,3 +17,16 @@ def get_conversation(user):
         .prefetch_related(
             Prefetch('replies', queryset=Message.objects.select_related('sender', 'receiver'))
         )
+
+@login_required
+def send_message(request):
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = request.user
+            message.save()
+            return redirect('inbox')  # or wherever
+    else:
+        form = MessageForm()
+    return render(request, 'messaging/send_message.html', {'form': form})
